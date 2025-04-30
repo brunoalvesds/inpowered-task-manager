c5be7e0cc4294d4159487c65b85adfe2ce35756b
Bruno <brunoalves.ds95@gmail.com>
2025-04-29 21:25:43 -0300

    Project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TaskListPageComponent } from './pages/task-list-page/task-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
